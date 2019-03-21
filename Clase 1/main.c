fc33a559960bb91bb81b6a318db2de7d6738d750
alumno <alumno>
2019-03-21 10:39:11 -0300

    Recreo

--- Clase 1/main.c
+++ Clase 1/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// \t = tabular \n = saltear renglon    alt 92 = "\"
+// printf %d para mostrar entero
+// char* direccion del caracter
+// & = direccion (siempre)
 int main()
 {
-    printf("Hello world!\n");
+    int edad;
+    float altura;
+
+    printf("\nIngrese su edad: ");
+    scanf("%d",&edad);
+
+    printf("\nIngrese su altura: ");
+    scanf("%f",&altura);
+
+    printf("\n    Resultados\n");
+    printf("\nLa edad es: %d",edad);
+    printf("\nLa altura es: %.1f \n",altura);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
