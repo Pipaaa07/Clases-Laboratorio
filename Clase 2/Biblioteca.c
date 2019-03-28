c4073dadf785b156ddeab8de82a6d690cff8a8e0
Francisco004 <fran_25_02@hotmail.com>
2019-03-28 10:36:43 -0300

    Recreo 28/3

---
+++ Clase 2/Biblioteca.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Biblioteca.h"
+int saludar()
+{
+    printf("Hola biblioteca");
+    return 0;
+}
+
+int dividir(int numero)
+{
+    int respuesta;
+    int resultado;
+
+    respuesta=(esDistintoACero(numero));
+
+    if (respuesta==1)
+    {
+        resultado=numero/2;
+    }
+    return respuesta;
+}
+int esDistintoACero(int numero)
+{
+    if(numero!=0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int factorial (int numero)
+{
+    int respuesta;
+    if(numero==1)
+    {
+        return 1;
+    }
+    respuesta=numero*factorial(numero-1);
+    return respuesta;
+}
+
+int cambiarValor(int Valor)
+{
+    Valor=999;
+    return 1;
+}
+
+int cambiarValorReferencia(int* Valor)
+{
+    printf("El valor es: %d",*Valor);
+    *Valor=999;
+
+    return 1;
+}
+
---
+++ Clase 2/Biblioteca.h
@@ -0,0 +1,12 @@
+#ifndef BIBLIOTECA_H_INCLUDED
+#define BIBLIOTECA_H_INCLUDED
+int saludar();
+
+int dividir(int numero);
+int esDistintoACero(int numero);
+int factorial (int numero);
+int cambiarValor(int Valor);
+int cambiarValorReferencia(int* Valor);
+
+
+#endif // BIBLIOTECA_H_INCLUDED
